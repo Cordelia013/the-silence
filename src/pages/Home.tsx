63a952e15ebd3dd1665a3bdc3fdd25e9ec933008
Cordelia013 <cordeliaguims@yahoo.com>
2025-06-20 12:05:23 +0200

    Modification de l'index.html pour changer le titre en "The Silence" et ajouter une police Google. Mise à jour de la configuration Tailwind pour inclure la police "Space Grotesk". Ajout de styles CSS personnalisés, y compris un style de barre de défilement et des animations. Intégration d'un composant HomeLoader dans la page d'accueil.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,10 @@
+import HomeLoader from "../layouts/HomeLoader";
+
 function Home() {
-  return <div className="p-8 text-2xl">Home page 🏠</div>;
+  return (
+    <div className="p-8 text-2xl">
+      <HomeLoader />
+    </div>
+  );
 }
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        space: ["'Space Grotesk'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
