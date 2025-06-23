17a98f2e9a126b2d05170e066c462d68f5d5fecd
Cordelia013 <64865633+Cordelia013@users.noreply.github.com>
2025-06-23 22:30:42 +0200

    Ajout des routes manquantes dans App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
-
-// ... autres imports
+import Events from "./pages/Events";
+import Ticketing from "./pages/Ticketing";
+import History from "./pages/History";
+import Info from "./pages/Info";
 
 function App() {
   return (
     <BrowserRouter>
-      <div  className="">
+      <div className="">
         <Routes>
           <Route path="/" element={<Home />} /> 
           <Route path="/gallery" element={<Gallery />} />
-
-
+          <Route path="/events" element={<Events />} />
+          <Route path="/ticketing" element={<Ticketing />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/info" element={<Info />} />
         </Routes>
-     
       </div>
     </BrowserRouter>
   );
 }
+
 export default App;
