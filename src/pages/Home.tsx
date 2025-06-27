f22427e00c84bd01a2b502cf2d91fa7821a4fbc5
Cordelia013 <64865633+Cordelia013@users.noreply.github.com>
2025-06-27 16:07:33 +0200

    Corriger la taille de police de la navigation dans Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,7 +49,7 @@ function Home() {
           position: 'relative'
         }}>
            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Navbar fontSize="24rem" gap="3.32px" />
+            <Navbar fontSize="3.84rem" gap="3.32px" />
           </div>
           <div style={{ 
             position: 'absolute',
